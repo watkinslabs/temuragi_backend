a16f77de95eb7a9390ce2da9e0261b37d87df233
Chris Watkins <Chris@watkinslabs.com>
2025-05-30 12:50:44 -0400

    agent

--- temuragi_agent/setup.py
+++ temuragi_agent/setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+from wl_version_manager import VersionManager
+
 setup(
     name="temuragi_agent",
-    version="0.1.0",
+    version=VersionManager.get_version(),
+    author="Chris Watkins",
+    author_email="chris@watkinslabs.com",
     packages=find_packages(),
+    long_description=long_description,
     include_package_data=True,
     package_data={"temuragi_agent": ["config.yaml"]},
     install_requires=[
